70184c6401abe5c1fca004eeb29ae42346c62676
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-07-20 08:21:37 +0900

    Proper status code for bad request

--- test/test.js
+++ test/test.js
@@ -24,3 +24,79 @@ test('test', t => {
     t.equal(result.statusCode, 200);
   });
 });
+
+test('test for wrong content-type', t => {
+  t.plan(9);
+
+  const pambda = jsonApi();
+
+  const lambda = pambda((event, context, callback) => {
+    t.fail('`next` lambda must not be called in this case');
+  });
+
+  /*
+   * Invalid content-type.
+   */
+  lambda({
+    path: '/',
+    headers: {
+      'Content-Type': ';;;',
+    },
+    body: '{}',
+  }, {}, (err, result) => {
+    t.error(err);
+    t.ok(result);
+    t.equal(result.statusCode, 400);
+  });
+
+  /*
+   * Missing content-type.
+   */
+  lambda({
+    path: '/',
+    headers: {
+    },
+    body: '{}',
+  }, {}, (err, result) => {
+    t.error(err);
+    t.ok(result);
+    t.equal(result.statusCode, 415);
+  });
+
+  /*
+   * Not supported content-type.
+   */
+  lambda({
+    path: '/',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: '{}',
+  }, {}, (err, result) => {
+    t.error(err);
+    t.ok(result);
+    t.equal(result.statusCode, 415);
+  });
+});
+
+test('test for invalid json', t => {
+  t.plan(3);
+
+  const pambda = jsonApi();
+
+  const lambda = pambda((event, context, callback) => {
+    t.fail('`next` lambda must not be called in this case');
+  });
+
+  lambda({
+    path: '/',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: '{x:1}',
+  }, {}, (err, result) => {
+    t.error(err);
+    t.ok(result);
+    t.equal(result.statusCode, 400);
+  });
+});
